00aae8299e25d2118ed573f6de9fff2f0cc649c7
KieranCHerman <54490438+KieranCHerman32@users.noreply.github.com>
2021-05-25 16:53:57 -0500

    Created child models for CharacterInfo

---
+++ src/app/types/deity.ts
@@ -0,0 +1,10 @@
+export interface Deity {
+  name: string;
+  titles: string[];
+  aliases: string[];
+  homePlane: string;
+  alignment: string;
+  portfolio: string[];
+  domains: string[];
+  settings: string[];
+}
