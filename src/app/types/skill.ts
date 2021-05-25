590674357dd30183235bd3682609bd72e6750044
KieranCHerman <54490438+KieranCHerman32@users.noreply.github.com>
2021-05-25 15:55:57 -0500

    Create skill.ts

---
+++ src/app/types/skill.ts
@@ -0,0 +1,13 @@
+export class Skill {
+  name: string;
+  keyAbility: string;
+
+  abilityMod: number;
+  ranks: number;
+  miscMod: number;
+  skillMod: number;
+
+  armorCheck: boolean;
+  useUntrained: boolean;
+  classSkill: boolean;
+}
