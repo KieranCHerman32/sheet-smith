df0b0bc6d0557080904bb665576adc94127f6b94
KieranCHerman <54490438+KieranCHerman32@users.noreply.github.com>
2021-05-25 16:37:41 -0500

    Create characterInfo.ts

---
+++ src/app/types/characterInfo.ts
@@ -0,0 +1,17 @@
+export class CharacterInfo {
+name: string;
+careerXp: number;
+classInfo: charClass[];
+race: Race;
+alignment: string;
+Deity: Deity;
+size: Size;
+age: number;
+pronouns: string[];
+height: string;
+weight: string;
+eyes: string;
+hair: string;
+skin: string;
+miscInfo: string;
+}
