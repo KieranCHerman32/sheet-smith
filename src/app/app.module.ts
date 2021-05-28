4743624c8fe68e33d57f299daf62e637eb8d557a
KieranCHerman <54490438+KieranCHerman32@users.noreply.github.com>
2021-05-27 21:43:44 -0500

    full refresh of angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AbilityScoresComponent } from './ability-scores/ability-scores.component';
+
 import { AppComponent } from './app.component';
-import { CharacterInfoComponent } from './character-info/character-info.component';
-import { SavingThrowsComponent } from './saving-throws/saving-throws.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CharacterInfoComponent,
-    AbilityScoresComponent,
-    SavingThrowsComponent,
-    TopNavComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatInputModule,
-    MatFormFieldModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
