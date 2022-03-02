3b44a1ecd352859e6088aee1ca089b8ccc4121d7
KieranCHerman32 <kc.herman32@gmail.com>
2022-03-02 16:21:16 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import {
-	getAnalytics,
-	provideAnalytics,
 	ScreenTrackingService,
 	UserTrackingService,
 } from '@angular/fire/analytics';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
-import {
-	getRemoteConfig,
-	provideRemoteConfig,
-} from '@angular/fire/remote-config';
-import { getStorage, provideStorage } from '@angular/fire/storage';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
@@ -63,7 +49,6 @@ import { SideNavContentComponent } from './side-nav-content/side-nav-content.com
 		ArmorComponent,
 	],
 	imports: [
-		// AnalyticsModule,
 		AngularFireModule.initializeApp(environment.firebase),
 		AngularFirestoreModule,
 		AppRoutingModule,
@@ -80,16 +65,6 @@ import { SideNavContentComponent } from './side-nav-content/side-nav-content.com
 		MatSidenavModule,
 		MatTableModule,
 		MatToolbarModule,
-		provideAnalytics(() => getAnalytics()),
-		provideAuth(() => getAuth()),
-		provideDatabase(() => getDatabase(initializeApp(environment.firebase))),
-		provideFirebaseApp(() => initializeApp(environment.firebase)),
-		provideFirestore(() => getFirestore()),
-		provideFunctions(() => getFunctions()),
-		provideMessaging(() => getMessaging()),
-		providePerformance(() => getPerformance()),
-		provideRemoteConfig(() => getRemoteConfig()),
-		provideStorage(() => getStorage()),
 	],
 	providers: [
 		ScreenTrackingService,
