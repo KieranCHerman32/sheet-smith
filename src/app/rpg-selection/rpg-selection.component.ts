e96b4666eaea3e45e7c2153ba5db96c1880f94fc
KieranCHerman32 <kc.herman32@gmail.com>
2022-03-22 12:39:39 -0500

    Update rpg-selection.component.ts

--- src/app/rpg-selection/rpg-selection.component.ts
+++ src/app/rpg-selection/rpg-selection.component.ts
@@ -1,5 +1,5 @@
-import { DataService } from './../services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../services/data.service';
 
 @Component({
 	selector: 'app-rpg-selection',
