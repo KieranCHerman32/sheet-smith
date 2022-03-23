7c90f5098785ede9f7fdfe73486ddf3bc573ee7e
KieranCHerman32 <kc.herman32@gmail.com>
2022-03-23 09:13:33 -0500

    Updated Linting spec

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from '@angular/core'
-import { ActivatedRoute, Router } from '@angular/router'
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class NavigationService {
-  constructor (private router: Router, private route: ActivatedRoute) {}
+	constructor(private router: Router, private route: ActivatedRoute) {}
 
-  to (path: string, relative: boolean = true) {
-    console.log(this.router.url)
-    if (relative === true) {
-      this.router.navigate([`/${path}`], { relativeTo: this.route })
-    } else {
-      this.router.navigate([`/${path}`])
-    }
-  }
+	to(path: string, relative: boolean = true) {
+		console.log(this.router.url);
+		if (relative === true) {
+			this.router.navigate([`/${path}`], { relativeTo: this.route });
+		} else {
+			this.router.navigate([`/${path}`]);
+		}
+	}
 
-  back (relative: boolean = true) {
-    if (relative === false) {
-      this.router.navigate(['..'])
-    } else {
-      this.router.navigate([''])
-    }
-  }
+	back(relative: boolean = true) {
+		if (relative === false) {
+			this.router.navigate(['..']);
+		} else {
+			this.router.navigate(['']);
+		}
+	}
 
-  getParam (param: any) {
-    return this.route.snapshot.paramMap.get(`${param}`)
-  }
+	getParam(param: any) {
+		return this.route.snapshot.paramMap.get(`${param}`);
+	}
 }
