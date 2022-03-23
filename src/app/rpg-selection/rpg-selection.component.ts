1c705579c15c6c1808c167445f34c175b1e87024
KieranCHerman32 <KieranCHerman32@users.noreply.github.com>
2022-03-23 14:23:59 +0000

    [MegaLinter] Apply linters fixes

--- src/app/rpg-selection/rpg-selection.component.ts
+++ src/app/rpg-selection/rpg-selection.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './../services/data.service';
+import { Component, OnInit } from '@angular/core'
+import { DataService } from './../services/data.service'
 
 @Component({
-	selector: 'app-rpg-selection',
-	templateUrl: './rpg-selection.component.html',
-	styleUrls: ['./rpg-selection.component.scss'],
+  selector: 'app-rpg-selection',
+  templateUrl: './rpg-selection.component.html',
+  styleUrls: ['./rpg-selection.component.scss']
 })
 export class RpgSelectionComponent implements OnInit {
 	sheetData: any = [];
-	constructor(private dataService: DataService) {}
+	constructor (private dataService: DataService) {}
 
-	ngOnInit(): void {
-		this.fetchSheetData();
+	ngOnInit (): void {
+	  this.fetchSheetData()
 	}
 
-	async fetchSheetData() {
-		this.sheetData = [];
-		this.dataService.rtGet('charSheets').forEach(sheet => {
-			this.sheetData = [...sheet];
-		});
+	async fetchSheetData () {
+	  this.sheetData = []
+	  this.dataService.rtGet('charSheets').forEach(sheet => {
+	    this.sheetData = [...sheet]
+	  })
 	}
 }
