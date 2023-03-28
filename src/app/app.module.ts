31a2a89cf47fd810eb792968ba9d84cd0dce19a2
KieranCHerman <54490438+KieranCHerman32@users.noreply.github.com>
2023-03-28 03:28:38 -0500

    Implementing snackbar for notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,51 +11,50 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
 import { MainnavComponent } from './mainnav/mainnav.component';
 import { RpgSelectionComponent } from './rpg-selection/rpg-selection.component';
 import { SideNavContentComponent } from './side-nav-content/side-nav-content.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		CharacterSheetComponent,
-		MainnavComponent,
-		RpgSelectionComponent,
-		SideNavContentComponent,
-	],
-	imports: [
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		BrowserModule.withServerTransition({ appId: 'serverApp' }),
-		FontAwesomeModule,
-		FormsModule,
-		MatButtonModule,
-		MatCardModule,
-		MatDividerModule,
-		MatExpansionModule,
-		MatFormFieldModule,
-		MatIconModule,
-		MatSelectModule,
-		MatSidenavModule,
-		MatTableModule,
-		MatToolbarModule,
-  provideFirebaseApp(() => initializeApp(environment.firebase)),
-  provideAuth(() => getAuth()),
-  provideDatabase(() => getDatabase()),
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    CharacterSheetComponent,
+    MainnavComponent,
+    RpgSelectionComponent,
+    SideNavContentComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    FontAwesomeModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,34 +1,58 @@
 import { DatePipe } from '@angular/common';
 import { inject, Injectable } from '@angular/core';
 import { Database, ref, set } from '@angular/fire/database';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class DataService {
   private database: Database = inject(Database);
 
-	constructor() {}
+  constructor(private snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'dismiss', {
+      duration: 3000,
+      panelClass: 'center',
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
 
   writeTest() {
     const date = Date.now();
     set(ref(this.database, `testing/${date}`), {
       Data: `TestRun ${new DatePipe('en-EN').transform(date)}`
-    });
+    }).then(() => {
+      this.openSnackBar('Saved Successfully!')
+      // Data saved successfully!
+    })
+      .catch((error: Error) => {
+        this.openSnackBar(`Error: ${error.message}`)
+        // The write failed...
+      });
   }
 
   // overwrites data at the specified location, including any child nodes.
   write(location: string, data: any) {
     set(ref(this.database, location), {
       data
-    });
+    }).then(() => {
+      this.openSnackBar('Saved Successfully!')
+      // Data saved successfully!
+    })
+      .catch((error: Error) => {
+        this.openSnackBar(`Error: ${error.message}`)
+        // The write failed...
+      });
   }
 
-  listen() {}
+  listen() { }
 
-  get() {}
+  get() { }
 
-  getOnce() {}
+  getOnce() { }
 
-  update() {}
+  update() { }
 }
